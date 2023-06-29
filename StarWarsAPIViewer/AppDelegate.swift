1b907a6c838e4bcfab3b4049cf180422ec9abf67
Dima Dima <natifeworkdima@gmail.com>
2023-06-29 19:41:57 +0100

    Run application through SwiftUI approach

--- StarWarsAPIViewer/AppDelegate.swift
+++ StarWarsAPIViewer/AppDelegate.swift
@@ -5,32 +5,24 @@
 //  Created by Scott Runciman on 20/07/2021.
 //
 
-import UIKit
+import SwiftUI
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+struct StarWarsAPIViewerApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            MainCoordinatorView(object: appDelegate.mainCoordinator)
+        }
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    let mainCoordinator = MainCoordinatorObject()
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        mainCoordinator.start()
+        return true
+    }
+}
